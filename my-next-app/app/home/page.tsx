467da19544ae54d21a1787ac2be21a943842288f
Kiran <kiran.sadara6730@gmail.com>
2026-01-19 21:15:55 +0530

    Pages added in Frontend

---
+++ my-next-app/app/home/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+
+export default function Home() {
+  return (
+   <div>
+    <h1>HomePage</h1>
+    <p>Welcome to the home page of the application.</p>
+   </div>
+  );
+}
---
+++ my-next-app/app/signin/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+
+export default function Signin() {
+  return (
+   <div>
+    <h1>Sign Up Page</h1>
+    <p>Create your account here.</p>
+   </div>
+  );
+}
