de288ff16bdd2ec2492d5ded22797428ad3ae927
Kiran <kiran.sadara6730@gmail.com>
2026-01-19 20:33:13 +0530

    Logger Added

--- api-gateway/app/controllers/Auth_Controller.js
+++ api-gateway/app/controllers/Auth_Controller.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import Signin_Service from '../services/Auth_Service.js'
 import { Signup_Service , Generate_Code_Service , Verify_Code_Service , Change_Password_Service } from '../services/Auth_Service.js';
-
+import { app_logger } from '../../utils/logger/app_logger.js';
 export const Signin_Controller = async(req,res)=>{
+
+    app_logger.info(`Entered into the Signin Controller`)
       
     try{
 
@@ -28,7 +30,8 @@ export const Signin_Controller = async(req,res)=>{
             secure:false,
             sameSite:"lax"
         })
-
+         
+        app_logger.info(`User logged in successfully , JWT token generated and set in cookies`)
         return res.status(200).json({
             message : "Login Successfull",
             token : token
@@ -37,6 +40,7 @@ export const Signin_Controller = async(req,res)=>{
     }
     catch(er){
          
+        app_logger.error(`Error in Signin Controller: ${er.message}`)
         if(er.message === 'Credentials are Wrong'){
             return res.status(404).json({
                 message : "Credentials are Wrong"
@@ -52,21 +56,21 @@ export const Signin_Controller = async(req,res)=>{
 
 
 export const Signup_Controller = async(req,res)=>{
-      
+    app_logger.info(`Entered into the Signup Controller`)
     try{
 
         const {email , username , password } = req.body;
 
         const user = await Signup_Service(email , username, password);
 
-
+            app_logger.info(`User Registered Successfully with email: ${email}`)
         return res.status(200).json({
             message : "User Registered Successfully."
         })
 
     }
     catch(er){
-
+        app_logger.error(`Error in Signup Controller: ${er.message}`)
         if(er.message === 'Email Not found'){
             return res.status(404).json({
                 message : "Email Not found"
@@ -88,20 +92,22 @@ export const Signup_Controller = async(req,res)=>{
 
 
 export const Generate_Code_Controller = async(req,res)=>{
-      
+    app_logger.info(`Entered into the Generate Code Controller`)
     try{
 
         const email =req.body;
 
         const message = await Generate_Code_Service(email);
-
+        
+        app_logger.info(`Verification code sent to email: ${email}`)
         return res.status(200).json({
             message : "Verification code sent successfully."
         })
 
     }
     catch(er){
-
+        
+        app_logger.error(`Error in Generate Code Controller: ${er.message}`)
         if(er.message === 'Email Not found'){
             return res.status(400).json({
                 message  :"Email Not found"
@@ -117,13 +123,14 @@ export const Generate_Code_Controller = async(req,res)=>{
 }
 
 export const Verify_Code_Controller = async(req,res)=>{
-      
+    
+    app_logger.info(`Entered into the Verify Code Controller`)
     try{
 
         const {email , code}   = req.body;
 
         const response = await Verify_Code_Service(email , code);
-
+            app_logger.info(`Verification code for email: ${email} verified successfully`)
         return res.status(200).json({
             message : "Code verified successfully"
         })
@@ -131,7 +138,7 @@ export const Verify_Code_Controller = async(req,res)=>{
     }
 
     catch(er){
-         
+         app_logger.error(`Error in Verify Code Controller: ${er.message}`)
         if(er.message === 'Email is wrong'){
             return res.status(400).json({
                 message : "Email is wrong"
@@ -158,20 +165,21 @@ export const Verify_Code_Controller = async(req,res)=>{
 }
 
 export const Change_Password_Controller = async(req,res)=>{
-     
+    
+    app_logger.info(`Entered into the Change Password Controller`)
     try{
 
         const {email , password}  = req.body;
 
         const response = await Change_Password_Service(email , password);
-
+        app_logger.info(`Password changed successfully for email: ${email}`)
         return res.status(200).json({
             message : "Password changed successfully"
         })
 
     }
     catch(er){
-         
+            app_logger.error(`Error in Change Password Controller: ${er.message}`)
         if(er.message === 'Email not Found'){
             return res.status(400).json({
                 message : "Email not Found"
--- api-gateway/app/middleware.js/Authentication_token.js
+++ api-gateway/app/middleware.js/Authentication_token.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
+import { app_logger } from '../../utils/logger/app_logger.js';
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 
 export const Authenticate_token = (req,res,next)=>{
+
+    app_logger.info(`Entered into the Authenticate token middleware`)
 
     const token = req.cookies?.token;
 
@@ -20,11 +23,12 @@ export const Authenticate_token = (req,res,next)=>{
 
         const decoded =jwt.verify(token , JWT_SECRET);
         req.user = decoded;
+        app_logger.info(`JWT payload fetched successfully`)
         next();
 
     }
     catch(er){
-         console.log("JWT ERROR:", err);
+        app_logger.info("JWT ERROR:", err);
     return res.status(401).json({ message: "Invalid or expired token" });
     }
 }
